7419eab05c07d355a39e8aef49d226ac90298371
Ishmael <jaswged@gmail.com>
2019-12-08 18:44:28 -0600

    Spawn multiple players based on input

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -5,31 +5,41 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Composites;
 
 public class CubeController : MonoBehaviour {
-    private Vector2 i_movement;
-    private float moveSpeed;
+    private Vector2 _iMovement;
+    private float _moveSpeed = 10f;
     
-    private void OnMoveStick(InputValue value) {
+    private void OnMove(InputValue value) {
         Debug.Log($"Moving: {value.Get<Vector2>()}");
         
-        i_movement = value.Get<Vector2>();
+        _iMovement = value.Get<Vector2>();
     }
     
-    private void OnMoveUp() {
+    private void OnJump() {
         Debug.Log("Moving up");
         transform.Translate(transform.up);
     }
     
-    private void OnMoveDown() {
+    private void OnPunch() {
+        Debug.Log("Moving up");
+        transform.Translate(transform.up);
+    }
+    
+    private void OnBlock() {
+        Debug.Log("Moving up");
+        transform.Translate(transform.up);
+    }
+    
+    private void OnKick() {
         Debug.Log("Moving down");
         transform.Translate(-transform.up);
     }
 
-    void Update() {
+    private void Update() {
         Move();
     }
 
     private void Move() {
-        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * moveSpeed * Time.deltaTime;
+        var movement = _moveSpeed * Time.deltaTime * _iMovement;
         transform.Translate(movement);
     }
 }
