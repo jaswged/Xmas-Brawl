999a5e819fdbe7cd373b19d65539c1a44a170779
Ishmael <jaswged@gmail.com>
2019-12-08 18:39:12 -0600

    Add input and dependencies for 2d bone animation

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
+
+public class CubeController : MonoBehaviour {
+    private Vector2 i_movement;
+    private float moveSpeed;
+    
+    private void OnMoveStick(InputValue value) {
+        Debug.Log($"Moving: {value.Get<Vector2>()}");
+        
+        i_movement = value.Get<Vector2>();
+    }
+    
+    private void OnMoveUp() {
+        Debug.Log("Moving up");
+        transform.Translate(transform.up);
+    }
+    
+    private void OnMoveDown() {
+        Debug.Log("Moving down");
+        transform.Translate(-transform.up);
+    }
+
+    void Update() {
+        Move();
+    }
+
+    private void Move() {
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
